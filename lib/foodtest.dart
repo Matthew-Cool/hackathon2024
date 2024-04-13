7b357dd7bc680ac330f1d9362f637406e782a686
Santosh Vangoor <svangoor@uark.edu>
2024-04-12 22:20:12 -0500

    Flutter Barcode Scanner

--- lib/foodtest.dart
+++ lib/foodtest.dart
@@ -1,18 +1,32 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class Page1 extends StatelessWidget {
   const Page1({
     super.key,
   });
 
-  @override
-  Widget build(BuildContext context) {
-    return const CupertinoPageScaffold(
-      navigationBar:
-          CupertinoNavigationBar(), //nav bar at the top of screen
-      child: Center(
-        child: Text("Page1"),
-      ), //text in the middle of screen
+  Future<void> scanBarcodeNormal() async {
+    String barcodeScanRes;
+    barcodeScanRes = await FlutterBarcodeScanner.scanBarcode(
+      '#ff6666',
+      'Cancel',
+      true,
+      ScanMode.BARCODE,
     );
+    debugPrint(barcodeScanRes);
   }
+
+  @override
+  Widget build(BuildContext context) => CupertinoPageScaffold(
+    child: Flex(
+      direction: Axis.vertical,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        ElevatedButton(onPressed: scanBarcodeNormal,
+        child: const Text('Start Scan'),
+        ),
+      ],
+    ),
+  );
 }
