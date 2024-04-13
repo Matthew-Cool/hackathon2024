0a9da5cbf801a6830e6251ea137e6bb409b658ed
asalazar052 <asalazar052@icloud.com>
2024-04-12 22:45:00 -0500

    Calorie Bar

--- lib/Custom Widgets/caloriebar.dart
+++ lib/Custom Widgets/caloriebar.dart
@@ -11,16 +11,7 @@ class _CalorieBarState extends State<CalorieBar> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      decoration: BoxDecoration(),
-      alignment: Alignment.center,
-      margin: EdgeInsets.all(8.0),
-      width: 40,
-      height: 40,
-      child: const Text(
-          style: TextStyle(
-            color: Color.fromARGB(225, 142, 14, 14),
-          ),
-          "Hello world"),
+      child: Center(child: Text("Hello"),),
     );
   }
 }
