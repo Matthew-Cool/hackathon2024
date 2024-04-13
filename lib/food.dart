762a5a4ef736c671e74b1f3a0e28972560d5592a
Santosh Vangoor <svangoor@uark.edu>
2024-04-13 00:20:24 -0500

    food fix up

--- lib/food.dart
+++ lib/food.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
 
 class Food extends StatelessWidget {
   const Food({
@@ -8,29 +7,10 @@ class Food extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return CupertinoPageScaffold(
-      navigationBar: const CupertinoNavigationBar(), //nav bar at the top of screen
+    return const CupertinoPageScaffold(
+      navigationBar: CupertinoNavigationBar(), //nav bar at the top of screen
       child: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            TextField(
-              decoration: const InputDecoration(
-                labelText: 'Enter Food Name',
-              ),
-              onChanged: (value) {
-                
-              },
-            ),
-            const SizedBox(height: 20),
-            CupertinoButton(
-              child: const Text('Submit'),
-              onPressed: () {
-
-              },
-            ),
-          ]
-        )
+        child: Text("Food"),
       ), //text in the middle of screen
     );
   }
