a230a46bd36d15843bd9d8a44895b6760cd56d2f
Matthew-Cool <mww6121@gmail.com>
2024-04-13 05:37:35 -0500

    added expand to workouts to show cals

--- lib/Custom Widgets/workouts.dart
+++ lib/Custom Widgets/workouts.dart
@@ -12,15 +12,17 @@ class Workout extends StatefulWidget {
 }
 
 class _WorkoutState extends State<Workout> {  
+  bool _isExpanded = false;
+  
   @override
   Widget build(BuildContext context) {
     return CupertinoButton(
       padding: const EdgeInsets.all(5),
-      onPressed: () {},
+      onPressed: () => setState(() => _isExpanded = !_isExpanded),
       child: Container(
         padding: const EdgeInsets.all(5),
         width: 400,
-        height: 50,
+        height: _isExpanded ? 90 : 50,
         decoration: BoxDecoration(
             borderRadius: const BorderRadius.all(
               Radius.circular(10),
@@ -28,11 +30,33 @@ class _WorkoutState extends State<Workout> {
             border: Border.all(width: 5),
             color: Color.fromARGB(217, 251, 89, 89)),
         //margin: const EdgeInsets.all(),
-        child: Text(
-            style: const TextStyle(
-                fontSize: 20, color: Color.fromARGB(255, 0, 0, 0)),
-            widget.name),
+        child: _isExpanded ? allTheInfo() : onlyTitle(),
       ),
+    );
+  }
+
+  Widget onlyTitle(){
+    return Text(
+    style: const TextStyle(
+      fontWeight: FontWeight.w600,
+      fontSize: 20, color: Color.fromARGB(255, 255, 255, 255)),
+    widget.name);
+  }
+
+  Widget allTheInfo(){
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          style: const TextStyle(
+            fontWeight: FontWeight.w600,
+            fontSize: 20, color: Color.fromARGB(255, 255, 255, 255)),
+          widget.name
+        ),
+        const SizedBox(height: 20),
+        Text('${widget.estimatedCalBurned} Estimated Calories Burned', style: const TextStyle(
+            fontSize: 15, color: Color.fromARGB(255, 255, 255, 255)),)
+      ],
     );
   }
 }
