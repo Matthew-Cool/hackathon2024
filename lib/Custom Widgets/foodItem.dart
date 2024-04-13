af97a86d96ed8d1d3b606eb6da9d890e8403e1f5
Matthew-Cool <mww6121@gmail.com>
2024-04-13 06:44:33 -0500

    added search and expand support to food items

--- lib/Custom Widgets/foodItem.dart
+++ lib/Custom Widgets/foodItem.dart
@@ -14,15 +14,20 @@ class FoodItem extends StatefulWidget {
 }
 
 class _FoodItemState extends State<FoodItem> {  
+  bool _isExpanded = false;
+  
   @override
   Widget build(BuildContext context) {
     return CupertinoButton(
       padding: const EdgeInsets.all(5),
-      onPressed: () {g_calorie += widget.calories;},
+      onPressed: () {
+        g_calorie += widget.calories;
+        setState(() => _isExpanded = !_isExpanded);
+      },
       child: Container(
         padding: const EdgeInsets.all(5),
         width: 400,
-        height: 50,
+        height: _isExpanded ? 85 : 50,
         decoration: BoxDecoration(
             borderRadius: const BorderRadius.all(
               Radius.circular(10),
@@ -30,11 +35,34 @@ class _FoodItemState extends State<FoodItem> {
             border: Border.all(width: 5),
             color: Color.fromARGB(217, 121, 209, 96)),
         //margin: const EdgeInsets.all(),
-        child: Text(
-            style: const TextStyle(
-                fontSize: 20, color: Color.fromARGB(255, 53, 50, 50)),
-            widget.food),
+        child: _isExpanded ? allTheInfo() : onlyTitle(),
       ),
+    );
+  }
+
+  Widget onlyTitle(){
+    return Text(
+    style: const TextStyle(
+      fontWeight: FontWeight.w600,
+      fontSize: 20, color: Color.fromARGB(255, 0, 0, 0)),
+    widget.food);
+  }
+
+  Widget allTheInfo(){
+    int cal = widget.calories.toInt();
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          style: const TextStyle(
+            fontWeight: FontWeight.w600,
+            fontSize: 20, color: Color.fromARGB(255, 0, 0, 0)),
+          widget.food
+        ),
+        const SizedBox(height: 20),
+        Text('${cal} Estimated Calories', style: const TextStyle(
+            fontSize: 15, color: Color.fromARGB(255, 0, 0, 0)),)
+      ],
     );
   }
 }
