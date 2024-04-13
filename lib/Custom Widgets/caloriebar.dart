c8d9c2acd0e0d1ae2bf421166b5f5f2ff9d4e1a8
asalazar052 <asalazar052@icloud.com>
2024-04-13 00:50:17 -0500

    Added foodItem widget

--- lib/Custom Widgets/caloriebar.dart
+++ lib/Custom Widgets/caloriebar.dart
@@ -11,16 +11,16 @@ class _CalorieBarState extends State<CalorieBar> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      margin: EdgeInsets.all(30),
+      margin: const EdgeInsets.all(20),
       width: 350,
       height: 40,
       decoration: BoxDecoration(
-        borderRadius: BorderRadius.all(
-          Radius.circular(3.0),
+        borderRadius: const BorderRadius.all(
+          Radius.circular(10.0),
         ),
         border: Border.all(color: Color.fromARGB(243, 235, 235, 235), width: 4),
       ),
-      child: Center(
+      child: const Center(
         child: Text("Calories"),
       ),
     );
