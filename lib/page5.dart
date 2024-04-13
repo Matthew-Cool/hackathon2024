4714af09a4796f538c5396543f8c86113410a9aa
hpattillo <pattillo@uark.edu>
2024-04-13 09:10:38 -0500

    page5 done

--- lib/page5.dart
+++ lib/page5.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:hackathon_app/Custom%20Widgets/awards.dart';
+import 'package:hackathon_app/main.dart';
 
 
 class Page5 extends StatelessWidget {
@@ -8,16 +9,39 @@ class Page5 extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const CupertinoPageScaffold(
-      child: Center(
-        child: Text("Hello"),
+    return CupertinoPageScaffold(
+      navigationBar: CupertinoNavigationBar(
+        middle: Container(
+          padding: const EdgeInsets.all(5),
+          child: const Image(
+            image: AssetImage('assets/images/appBarIcon.png'),
+          ),
+        ),
+        
       ),
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Container(padding: EdgeInsets.only(top: 200), child: Text("Calories", style: TextStyle(fontSize: 20))),
+              Image(height: 440, width: 500, fit: BoxFit.fill,
+                image: AssetImage('assets/images/BRONZEMedal.png'),),
+
+            ]
+          
+        ),
+      )
     );
   }
+
+
+  
 }
 
 
 
-//String medal = findMedal("Calories", 11);
+//String medal = getMedal("Calories", 11); 
+//(what the medal is for, what the score is)
 
 
